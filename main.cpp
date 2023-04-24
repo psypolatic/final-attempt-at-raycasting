98120d6f5cb932d31cadf24e5d2dc4cc7bc38627
Joselopez42 <6350748-Joselopez42@users.noreply.replit.com>
2023-04-24 17:58:30 +0000

    a

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <SDL2/SDL.h>
 #include <iostream>//I code in c mostly so i prefer stdio.h but ¯\_(ツ)_/¯
 
-
+//a
 //the worlds most boring main function
 //even a helloworld function is more exciting
 int main() {
